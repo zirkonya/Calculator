066dc9434a8c8d20950a3df696200602939771b7
zirkonya <matier.oxirion@gmail.com>
2022-03-03 18:57:13 +0100

    ajout Operator

---
+++ app/src/main/java/fr/upjv/calculator/calcul/Operator.java
@@ -0,0 +1,25 @@
+package fr.upjv.calculator.calcul;
+
+public enum Operator {
+    ADD('+', false),
+    SUBTRACT('-', false),
+    MULTIPLY('*', true),
+    DIVIDE('/', true),
+    AND('&', false),
+    OR('|', false);
+
+    private char symbol;
+    private boolean priority;
+    Operator(char symbol, boolean priority) {
+        this.symbol = symbol;
+        this.priority = priority;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public boolean isPriority() {
+        return priority;
+    }
+}
